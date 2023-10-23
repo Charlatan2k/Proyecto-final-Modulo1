eddc38112815d065b0061fdcf3c07fcb00ac36d7
DrNueli <147388538+DrNueli@users.noreply.github.com>
2023-10-23 12:00:23 +0200

    Delete testProfessional.js

--- testProfessional.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Professional_1 = require("./Professional");
-var JW = new Professional_1.Professional("Reeves", 45, 75, 170, false, "USA", 1, "actor");
-JW.showAttributes();
